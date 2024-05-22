eb5f62db4befc20c4131a525e60424b8e92716dd
William Shepherd <williamrshepherd@gmail.com>
2024-05-22 16:31:10 -0500

    Dont use dotenv

--- src/DevCycle.php
+++ src/DevCycle.php
@@ -4,20 +4,16 @@ namespace DevCycle;
 
 use DevCycle\Api\DevCycleClient;
 use DevCycle\Model\DevCycleOptions;
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../../../');
-$dotenv->load();
 
 class DevCycle
 {
   public $devcycleClient;
 
-  function __construct()
+  function __construct($server_sdk_key)
   {
     $devcycleOptions = new DevCycleOptions();
     $this->devcycleClient = new DevCycleClient(
-      sdkKey: $_ENV["DEVCYCLE_SERVER_SDK_KEY"],
+      sdkKey: $server_sdk_key,
       dvcOptions: $devcycleOptions
     );
   }
